421e57f4be4efd7a6bebd79b4a39b5f109dfdad6
Foiyoh <marius.wilmar@gmail.com>
2025-01-25 21:34:03 +0100

    main loop

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,46 @@
 const std = @import("std");
+const eql = std.mem.eql;
+
+const menu_string: []const u8 = "--- Todo-List ---\n1 - Show todos\n2 - Complete todo\n3 - Create todo\n4 - Exit\nEnter number: ";
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
+    try inputLoop();
+}
+
+pub fn inputLoop() !void {
     const stdout = std.io.getStdOut().writer();
+
+    while (true) {
+        try stdout.writeAll(menu_string);
+        const input_value = std.mem.trim(u8, try readNextLine(), "\r\n");
+        defer std.heap.page_allocator.free(input_value);
+
+        if (eql(u8, input_value, "1")) {
+            //TODO
+        } else if (eql(u8, input_value, "2")) {
+            //TODO
+        } else if (eql(u8, input_value, "3")) {
+            //TODO
+        } else if (eql(u8, input_value, "4")) {
+            try stdout.writeAll("Exit\n");
+            break;
+        } else {
+            try stdout.writeAll("Invalid input\n");
+        }
+    }
+}
+
+pub fn readNextLine() ![]u8 {
+    const allocator = std.heap.page_allocator;
     const cin = std.io.getStdIn().reader();
 
-    // Dynamischer Puffer für die Eingabe
-    var list = std.ArrayList(u8).init(allocator);
-    defer list.deinit(); // Speicher freigeben
+    var input_string = std.ArrayList(u8).init(allocator);
+    defer input_string.deinit();
 
-    // Eingabeaufforderung
-    try stdout.writeAll("Gib etwas ein (Beenden mit Enter): ");
-
-    // Zeichenweise lesen bis zum Zeilenumbruch
     while (true) {
-        const byte = try cin.readByte();
-        if (byte == '\n') break; // Beende die Schleife bei Zeilenumbruch
-        try list.append(byte); // Füge das Zeichen zum Puffer hinzu
+        const next_char: u8 = try cin.readByte();
+        if (next_char == '\n') break;
+        try input_string.append(next_char);
     }
-
-    // Ausgabe der gesamten Eingabe
-    std.debug.print("Du hast eingegeben: {s}\n", .{list.items});
+    return input_string.toOwnedSlice();
 }
